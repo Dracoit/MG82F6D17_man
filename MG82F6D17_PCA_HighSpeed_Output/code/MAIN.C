fb64b36a0142b6bb32f60e65c8a4a13c44435b87
Dracoit <52192140+Dracoit@users.noreply.github.com>
2025-12-07 00:52:40 +0530

    Add files via upload

---
+++ MG82F6D17_PCA_HighSpeed_Output/code/MAIN.C
@@ -0,0 +1,352 @@
+///*********************************************************************
+//    Project:MG82F6D17-DEMO
+//    Author:LZD
+//			MG82F6D17 SSOP20_V10 EV Board (TH194A)
+//			CpuCLK=12MHz, SysCLK=12MHz
+//	Description:
+//			CH0,CH2 Output CEX0(P22),CEX2(P24)
+//	Note:
+//
+//    Creat time::
+//    Modify::
+//    
+//*********************************************************************/
+#define _MAIN_C
+
+#include <Intrins.h>
+#include <Absacc.h>
+
+#include <Stdio.h>  // for printf
+
+#include ".\include\REG_MG82F6D17.H"
+#include ".\include\Type.h"
+#include ".\include\API_Macro_MG82F6D17.H"
+#include ".\include\API_Uart_BRGRL_MG82F6D17.H"
+
+
+///*************************************************
+//Set SysClk (MAX.50MHz) (MAX.50MHz)
+//Selection: 
+//	11059200,12000000,
+//	22118400,24000000,
+//	29491200,32000000,
+//	44236800,48000000
+//*************************************************/
+#define MCU_SYSCLK		12000000
+/*************************************************/
+///*************************************************
+//set CpuClk (MAX.36MHz)
+//	1) CpuCLK=SysCLK
+//	2) CpuClk=SysClk/2
+//*************************************************/
+#define MCU_CPUCLK		(MCU_SYSCLK)
+//#define MCU_CPUCLK		(MCU_SYSCLK/2)
+
+#define TIMER_1T_1ms_TH	((65536-(u16)(float)(1000*((float)(MCU_SYSCLK)/(float)(1000000)))) /256) 			
+#define TIMER_1T_1ms_TL	((65536-(u16)(float)(1000*((float)(MCU_SYSCLK)/(float)(1000000)))) %256)
+
+#define TIMER_12T_1ms_TH	((65536-(u16)(float)(1000*((float)(MCU_SYSCLK)/(float)(12000000)))) /256) 			
+#define TIMER_12T_1ms_TL	((65536-(u16)(float)(1000*((float)(MCU_SYSCLK)/(float)(12000000)))) %256)
+#define TIMER_12T_10mS_TH	((65536-(u16)(float)(10000*((float)(MCU_SYSCLK)/12/(float)(1000000)))) /256) 			
+#define TIMER_12T_10ms_TL	((65536-(u16)(float)(10000*((float)(MCU_SYSCLK)/12/(float)(1000000)))) %256)
+
+#define LED_G_0		P33
+#define LED_R		P34
+#define LED_G_1		P35
+
+
+
+///*************************************************
+//Function:     	void DelayXus(u16 xUs)
+//Description:   	dealy, unit:us
+//Input:     		u8 Us -> *1us  (1~255)
+//Output:     
+//*************************************************/
+void DelayXus(u8 xUs)
+{
+	while(xUs!=0)
+	{
+#if (MCU_CPUCLK>=11059200)
+		_nop_();
+#endif
+#if (MCU_CPUCLK>=14745600)
+		_nop_();
+		_nop_();
+		_nop_();
+		_nop_();
+#endif
+#if (MCU_CPUCLK>=16000000)
+		_nop_();
+#endif
+
+#if (MCU_CPUCLK>=22118400)
+		_nop_();
+		_nop_();
+		_nop_();
+		_nop_();
+		_nop_();
+		_nop_();
+#endif
+#if (MCU_CPUCLK>=24000000)
+		_nop_();
+		_nop_();
+#endif		
+#if (MCU_CPUCLK>=29491200)
+		_nop_();
+		_nop_();
+		_nop_();
+		_nop_();
+		_nop_();
+		_nop_();
+#endif
+#if (MCU_CPUCLK>=32000000)
+		_nop_();
+		_nop_();
+#endif
+
+		xUs--;
+	}
+}
+
+///*************************************************
+//Function:     	void DelayXms(u16 xMs)
+//Description:    dealy, unit:ms
+//Input:     		u16 xMs -> *1ms  (1~65535)
+//Output:     
+//*************************************************/
+void DelayXms(u16 xMs)
+{
+	while(xMs!=0)
+	{
+		CLRWDT();
+		DelayXus(200);
+		DelayXus(200);
+		DelayXus(200);
+		DelayXus(200);
+		DelayXus(200);
+		xMs--;
+		
+	}
+}
+
+///***********************************************************************************
+//Function:   	void InitPort()
+//Description:	Initialize IO Port
+//Input:   
+//Output:   		
+//*************************************************************************************/
+void InitPort(void)
+{
+	PORT_SetP3QuasiBi(BIT0|BIT1|BIT3|BIT4|BIT5);		// set P30,P31,P33,P34,P35 as Quasi-Bidirectional
+	PORT_SetP2PushPull(BIT2|BIT4);						// set P22,P24 as push-pull for output
+}
+
+
+///***********************************************************************************
+//Function:   	void InitPCA_HighSpeed(void)
+//Description:    Initialize PCA for High speed output
+//Input:   
+//Output:     
+//*************************************************************************************/
+void InitPCA_HighSpeed(void)
+{
+	PCA_SetCLOCK_SYSCLK();							// PCA clock: SysClk
+	
+	PCA_CH0_SetMode_16BitHighSpeedOutput();			// set CH0 for high speed output
+	PCA_CH2_SetMode_16BitHighSpeedOutput();			// set CH2 for high speed output
+	
+	PCA_SetCounter(0);
+	PCA_SetCounterReload(0);
+
+	PCA_CH0_SetValue_L(0x00);
+	PCA_CH0_SetValue_H(0x40);
+
+	PCA_CH2_SetValue_L(0x00);
+	PCA_CH2_SetValue_H(0x8);
+
+	PCA_SetCEX0CEX2CEX4_P22P24P17();				// CEX0/CEX2/CEX4 pin: P22/P24/P17
+
+	PCA_SetPWM0_EnOutput();							// enable CEX0 PWM0 output
+	PCA_SetPWM2_EnOutput();							// enable CEX2 PWM2 output
+	
+	
+	PCA_EnPCACounter();								// Enable PCA counter
+		
+}
+
+
+///***********************************************************************************
+//Function:   	void InitClock()
+//Description:	Initialize clock
+//Input:   
+//Output:   		
+//*************************************************************************************/
+void InitClock(void)
+{
+#if (MCU_SYSCLK==11059200)
+#if (MCU_CPUCLK==MCU_SYSCLK)
+	// SysClk=11.0592MHz CpuClk=11.0592MHz
+	CLK_SetCKCON0(IHRCO_110592MHz|CPUCLK_SYSCLK_DIV_1|SYSCLK_MCKDO_DIV_1);
+	
+#else
+	// SysClk=11.0592MHz CpuClk=5.5296MHz
+	CLK_SetCKCON0(IHRCO_110592MHz|CPUCLK_SYSCLK_DIV_2|SYSCLK_MCKDO_DIV_1);
+#endif
+#endif
+
+#if (MCU_SYSCLK==12000000)
+#if (MCU_CPUCLK==MCU_SYSCLK)
+	// SysClk=12MHz CpuClk=12MHz
+	CLK_SetCKCON0(IHRCO_12MHz|CPUCLK_SYSCLK_DIV_1|SYSCLK_MCKDO_DIV_1);
+	
+#else
+	// SysClk=12MHz CpuClk=6MHz
+	CLK_SetCKCON0(IHRCO_12MHz|CPUCLK_SYSCLK_DIV_2|SYSCLK_MCKDO_DIV_1);
+#endif
+#endif
+
+#if (MCU_SYSCLK==22118400)
+#if (MCU_CPUCLK==MCU_SYSCLK)
+	// SysClk=22.1184MHz CpuClk=22.1184MHz
+	CLK_SetCKCON0(IHRCO_110592MHz|CPUCLK_SYSCLK_DIV_1|SYSCLK_MCKDO_DIV_1|ENABLE_CKM|CKM_OSCIN_DIV_2);
+	DelayXus(100);
+	// IHRCO, MCK=CKMIx4, OSCin=IHRCO
+	CLK_SetCKCON2(ENABLE_IHRCO|MCK_CKMI_X4|OSCIn_IHRCO);
+#else
+	// SysClk=22.1184MHz CpuClk=11.0592MHz
+	CLK_SetCKCON0(IHRCO_110592MHz|CPUCLK_SYSCLK_DIV_2|SYSCLK_MCKDO_DIV_1|ENABLE_CKM|CKM_OSCIN_DIV_2);
+	DelayXus(100);
+	// IHRCO, MCK=CKMIx4, OSCin=IHRCO
+	CLK_SetCKCON2(ENABLE_IHRCO|MCK_CKMI_X4|OSCIn_IHRCO);
+#endif
+#endif
+
+#if (MCU_SYSCLK==24000000)
+#if (MCU_CPUCLK==MCU_SYSCLK)
+	// SysClk=24MHz CpuClk=24MHz
+	CLK_SetCKCON0(IHRCO_12MHz|CPUCLK_SYSCLK_DIV_1|SYSCLK_MCKDO_DIV_1|ENABLE_CKM|CKM_OSCIN_DIV_2);
+	DelayXus(100);
+	// IHRCO, MCK=CKMIx4, OSCin=IHRCO
+	CLK_SetCKCON2(ENABLE_IHRCO|MCK_CKMI_X4|OSCIn_IHRCO);
+#else
+	// SysClk=24MHz CpuClk=12MHz
+	CLK_SetCKCON0(IHRCO_12MHz|CPUCLK_SYSCLK_DIV_2|SYSCLK_MCKDO_DIV_1|ENABLE_CKM|CKM_OSCIN_DIV_2);
+	DelayXus(100);
+	// IHRCO, MCK=CKMIx4, OSCin=IHRCO
+	CLK_SetCKCON2(ENABLE_IHRCO|MCK_CKMI_X4|OSCIn_IHRCO);
+#endif
+#endif
+
+#if (MCU_SYSCLK==29491200)
+#if (MCU_CPUCLK==MCU_SYSCLK)
+	// Cpuclk high speed
+	CLK_SetCpuCLK_HighSpeed();
+	// SysClk=29.491200MHz CpuClk=29.491200MHz
+	CLK_SetCKCON0(IHRCO_110592MHz|CPUCLK_SYSCLK_DIV_1|SYSCLK_MCKDO_DIV_1|ENABLE_CKM|CKM_OSCIN_DIV_2);
+	DelayXus(100);
+	// IHRCO, MCK=CKMIx5.33, OSCin=IHRCO
+	CLK_SetCKCON2(ENABLE_IHRCO|MCK_CKMI_X533|OSCIn_IHRCO);
+#else
+	// SysClk=29.491200MHz CpuClk=14.7456MHz
+	CLK_SetCKCON0(IHRCO_110592MHz|CPUCLK_SYSCLK_DIV_2|SYSCLK_MCKDO_DIV_1|ENABLE_CKM|CKM_OSCIN_DIV_2);
+	DelayXus(100);
+	// IHRCO, MCK=CKMIx5.33, OSCin=IHRCO
+	CLK_SetCKCON2(ENABLE_IHRCO|MCK_CKMI_X533|OSCIn_IHRCO);
+#endif
+#endif
+
+#if (MCU_SYSCLK==32000000)
+#if (MCU_CPUCLK==MCU_SYSCLK)
+	// Cpuclk high speed
+	CLK_SetCpuCLK_HighSpeed();
+	// SysClk=32MHz CpuClk=32MHz
+	CLK_SetCKCON0(IHRCO_12MHz|CPUCLK_SYSCLK_DIV_1|SYSCLK_MCKDO_DIV_1|ENABLE_CKM|CKM_OSCIN_DIV_2);
+	DelayXus(100);
+	// IHRCO, MCK=CKMIx5.33, OSCin=IHRCO
+	CLK_SetCKCON2(ENABLE_IHRCO|MCK_CKMI_X533|OSCIn_IHRCO);
+#else
+	// SysClk=32MHz CpuClk=16MHz
+	CLK_SetCKCON0(IHRCO_12MHz|CPUCLK_SYSCLK_DIV_2|SYSCLK_MCKDO_DIV_1|ENABLE_CKM|CKM_OSCIN_DIV_2);
+	DelayXus(100);
+	// IHRCO, MCK=CKMIx5.33, OSCin=IHRCO
+	CLK_SetCKCON2(ENABLE_IHRCO|MCK_CKMI_X533|OSCIn_IHRCO);
+#endif
+#endif
+
+#if (MCU_SYSCLK==36000000)
+#if (MCU_CPUCLK==MCU_SYSCLK)
+	// Cpuclk high speed
+	CLK_SetCpuCLK_HighSpeed();
+	// CKMIx6,x8,x12
+	CLK_SetCKM_x6x8x12();	
+	// SysClk=36MHz CpuClk=18MHz
+	CLK_SetCKCON0(IHRCO_12MHz|CPUCLK_SYSCLK_DIV_1|SYSCLK_MCKDO_DIV_1|ENABLE_CKM|CKM_OSCIN_DIV_2);
+	DelayXus(100);
+	// IHRCO, MCK=CKMIx6, OSCin=IHRCO
+	CLK_SetCKCON2(ENABLE_IHRCO|MCK_CKMI_X4_X6|OSCIn_IHRCO);
+#else
+	// CKMIx6,x8,x12
+	CLK_SetCKM_x6x8x12();	
+	// SysClk=36MHz CpuClk=18MHz
+	CLK_SetCKCON0(IHRCO_12MHz|CPUCLK_SYSCLK_DIV_2|SYSCLK_MCKDO_DIV_1|ENABLE_CKM|CKM_OSCIN_DIV_2);
+	DelayXus(100);
+	// IHRCO, MCK=CKMIx6, OSCin=IHRCO
+	CLK_SetCKCON2(ENABLE_IHRCO|MCK_CKMI_X4_X6|OSCIn_IHRCO);
+#endif
+#endif
+
+
+#if (MCU_SYSCLK==44236800)
+	// SysClk=44.2368MHz CpuClk=22.1184MHz
+	CLK_SetCKCON0(IHRCO_110592MHz|CPUCLK_SYSCLK_DIV_1|SYSCLK_MCKDO_DIV_1|ENABLE_CKM|CKM_OSCIN_DIV_2);
+	DelayXus(100);
+	// IHRCO, MCK=CKMIx8, OSCin=IHRCO
+	CLK_SetCKCON2(ENABLE_IHRCO|MCK_CKMI_X8|OSCIn_IHRCO);
+#endif
+
+#if (MCU_SYSCLK==48000000)
+	// SysClk=48MHz CpuClk=24MHz
+	CLK_SetCKCON0(IHRCO_12MHz|CPUCLK_SYSCLK_DIV_2|SYSCLK_MCKDO_DIV_1|ENABLE_CKM|CKM_OSCIN_DIV_2);
+	DelayXus(100);
+	// IHRCO, MCK=CKMIx8, OSCin=IHRCO
+	CLK_SetCKCON2(ENABLE_IHRCO|MCK_CKMI_X8|OSCIn_IHRCO);
+#endif
+
+	// P60 Output MCK/4
+	//CLK_P60OC_MCKDiv4();
+}
+
+
+
+
+
+///***********************************************************************************
+//Function:       void InitSystem(void)
+//Description:    Initialize MCU
+//Input:   
+//Output:     
+//*************************************************************************************/
+void InitSystem(void)
+{
+	InitClock();
+	InitPort();
+	InitPCA_HighSpeed();
+}
+
+
+void main()
+{
+
+    InitSystem();
+	
+	LED_G_0=0;LED_R=0;LED_G_1=0;
+    DelayXms(1000);
+	LED_G_0=1;LED_R=1;LED_G_1=1;
+
+	while(1)
+    {
+    	DelayXms(200);
+		LED_G_0=!LED_G_0;
+    }
+}
+
---
+++ MG82F6D17_PCA_PWM_2AB_16bit/code/MAIN.C
@@ -0,0 +1,410 @@
+///*********************************************************************
+//    Project:MG82F6D17-DEMO
+//    Author:LZD
+//			MG82F6D17 SSOP20_V10 EV Board (TH194A)
+//			CpuCLK=12MHz, SysCLK=12MHz
+//	Description:
+//          PCA clock: 12MHz
+//			CH2 PWM output at PWM2A(P60) and PWM2B(P61)
+//			Edge Align,1000
+//			PWM Freq: 12MHz/(1000)=12KHz
+//	Note:
+//
+//    Creat time::
+//    Modify::
+//    
+//*********************************************************************/
+#define _MAIN_C
+
+#include <Intrins.h>
+#include <Absacc.h>
+
+#include <Stdio.h>  // for printf
+
+#include ".\include\REG_MG82F6D17.H"
+#include ".\include\Type.h"
+#include ".\include\API_Macro_MG82F6D17.H"
+#include ".\include\API_Uart_BRGRL_MG82F6D17.H"
+
+
+///*************************************************
+//Set SysClk (MAX.50MHz) (MAX.50MHz)
+//Selection: 
+//	11059200,12000000,
+//	22118400,24000000,
+//	29491200,32000000,
+//	44236800,48000000
+//*************************************************/
+#define MCU_SYSCLK		12000000
+/*************************************************/
+///*************************************************
+//set CpuClk (MAX.36MHz)
+//	1) CpuCLK=SysCLK
+//	2) CpuClk=SysClk/2
+//*************************************************/
+#define MCU_CPUCLK		(MCU_SYSCLK)
+//#define MCU_CPUCLK		(MCU_SYSCLK/2)
+
+#define TIMER_1T_1ms_TH	((65536-(u16)(float)(1000*((float)(MCU_SYSCLK)/(float)(1000000)))) /256) 			
+#define TIMER_1T_1ms_TL	((65536-(u16)(float)(1000*((float)(MCU_SYSCLK)/(float)(1000000)))) %256)
+
+#define TIMER_12T_1ms_TH	((65536-(u16)(float)(1000*((float)(MCU_SYSCLK)/(float)(12000000)))) /256) 			
+#define TIMER_12T_1ms_TL	((65536-(u16)(float)(1000*((float)(MCU_SYSCLK)/(float)(12000000)))) %256)
+
+#define LED_G_0		P33
+#define LED_R		P34
+#define LED_G_1		P35
+
+
+
+#define PWM_MIN			(0*1)
+#define PWM_MAX			(1000*1)
+#define PWM_3_4			(750*1)
+#define PWM_2_4			(500*1)
+#define PWM_1_4			(250*1)
+#define PWM_LOW			(40*1)
+#define PWM_HINT		(50*1)
+
+#define PCA_RELOAD		(PWM_MAX)
+
+#define PCA_C           (65536)       	
+
+#define PCA_CL(x)		(u8)((~(u16)x)+1) 
+#define PCA_CH(x)     	(u8)(((~(u16)x)+1)>>8)         
+
+idata WordTypeDef wDuty;
+u8 DutyFlag;
+
+
+///*************************************************
+//Function:     	void DelayXus(u16 xUs)
+//Description:   	dealy£¬unit:us
+//Input:     		u8 Us -> *1us  (1~255)
+//Output:     
+//*************************************************/
+void DelayXus(u8 xUs)
+{
+	while(xUs!=0)
+	{
+#if (MCU_CPUCLK>=11059200)
+		_nop_();
+#endif
+#if (MCU_CPUCLK>=14745600)
+		_nop_();
+		_nop_();
+		_nop_();
+		_nop_();
+#endif
+#if (MCU_CPUCLK>=16000000)
+		_nop_();
+#endif
+
+#if (MCU_CPUCLK>=22118400)
+		_nop_();
+		_nop_();
+		_nop_();
+		_nop_();
+		_nop_();
+		_nop_();
+#endif
+#if (MCU_CPUCLK>=24000000)
+		_nop_();
+		_nop_();
+#endif		
+#if (MCU_CPUCLK>=29491200)
+		_nop_();
+		_nop_();
+		_nop_();
+		_nop_();
+		_nop_();
+		_nop_();
+#endif
+#if (MCU_CPUCLK>=32000000)
+		_nop_();
+		_nop_();
+#endif
+
+		xUs--;
+	}
+}
+
+///*************************************************
+//Function:     	void DelayXms(u16 xMs)
+//Description:    dealy£¬unit:ms
+//Input:     		u16 xMs -> *1ms  (1~65535)
+//Output:     
+//*************************************************/
+void DelayXms(u16 xMs)
+{
+	while(xMs!=0)
+	{
+		CLRWDT();
+		DelayXus(200);
+		DelayXus(200);
+		DelayXus(200);
+		DelayXus(200);
+		DelayXus(200);
+		xMs--;
+		
+	}
+}
+
+///***********************************************************************************
+//Function:   	void InitPort()
+//Description:	Initialize IO Port
+//Input:   
+//Output:   		
+//*************************************************************************************/
+void InitPort(void)
+{
+	PORT_SetP3PushPull(BIT3|BIT4|BIT5);				// Set P33(CEX1),P34(CEX3),P35(CEX5) as push-pull
+	PORT_SetP6PushPull(BIT0|BIT1);						// set P60(PWM2A),P61(PWM2B) as push-pull for PWM output
+}
+
+
+///***********************************************************************************
+//Function:   	void InitPCA_PWM(void)
+//Description:    Initialize PCA for PWM
+//Input:   
+//Output:     
+//*************************************************************************************/
+void InitPCA_PWM(void)
+{
+	PCA_SetCLOCK_SYSCLK();				// PCA clock: SysClk
+	
+	PCA_CH2_SetMode_PWM();
+	PCA_CH3_SetMode_PWM();
+
+	PCA_CH2_SetPWM_16Bit();
+	PCA_CH3_SetPWM_16Bit();
+
+	PCA_CH23_SetPWM_EnBufferMode();		// CH2,CH3 Enable buffer mode
+
+	PCA_SetPWM_EdgeAligned();			// Edge-aligned
+
+	PCA_CH2_SetValue(PCA_CH(PWM_MIN),PCA_CL(PWM_MIN));
+	PCA_CH3_SetValue(PCA_CH(PWM_MIN),PCA_CL(PWM_MIN));
+
+	PCA_SetCounter(PCA_C-PCA_RELOAD);
+	PCA_SetCounterReload(PCA_C-PCA_RELOAD);
+
+
+	PCA_SetPWM2APWM2B_P60P61();			// set PWM2A:P60,PWM2B:P61
+
+	PCA_SetPWM2_DisOutput();			// disable PWM2 output
+	PCA_SetPWM2_2nd_EnOutput();			// enable PWM2A output
+	PCA_SetPWM2_3rd_EnOutput();			// enable PWM2B output
+
+	PCA_SetPWM3_DisOutput();			// disable PWM3 output
+
+	PCA_CF_DisInterrupt();				// Disable PCA CF interrupt
+
+	
+	
+	PCA_EnPCACounter();					// Enable PCA counter
+		
+}
+
+
+///***********************************************************************************
+//Function:   	void InitClock()
+//Description:	Initialize clock
+//Input:   
+//Output:   		
+//*************************************************************************************/
+void InitClock(void)
+{
+#if (MCU_SYSCLK==11059200)
+#if (MCU_CPUCLK==MCU_SYSCLK)
+	// SysClk=11.0592MHz CpuClk=11.0592MHz
+	CLK_SetCKCON0(IHRCO_110592MHz|CPUCLK_SYSCLK_DIV_1|SYSCLK_MCKDO_DIV_1);
+	
+#else
+	// SysClk=11.0592MHz CpuClk=5.5296MHz
+	CLK_SetCKCON0(IHRCO_110592MHz|CPUCLK_SYSCLK_DIV_2|SYSCLK_MCKDO_DIV_1);
+#endif
+#endif
+
+#if (MCU_SYSCLK==12000000)
+#if (MCU_CPUCLK==MCU_SYSCLK)
+	// SysClk=12MHz CpuClk=12MHz
+	CLK_SetCKCON0(IHRCO_12MHz|CPUCLK_SYSCLK_DIV_1|SYSCLK_MCKDO_DIV_1);
+	
+#else
+	// SysClk=12MHz CpuClk=6MHz
+	CLK_SetCKCON0(IHRCO_12MHz|CPUCLK_SYSCLK_DIV_2|SYSCLK_MCKDO_DIV_1);
+#endif
+#endif
+
+#if (MCU_SYSCLK==22118400)
+#if (MCU_CPUCLK==MCU_SYSCLK)
+	// SysClk=22.1184MHz CpuClk=22.1184MHz
+	CLK_SetCKCON0(IHRCO_110592MHz|CPUCLK_SYSCLK_DIV_1|SYSCLK_MCKDO_DIV_1|ENABLE_CKM|CKM_OSCIN_DIV_2);
+	DelayXus(100);
+	// IHRCO, MCK=CKMIx4, OSCin=IHRCO
+	CLK_SetCKCON2(ENABLE_IHRCO|MCK_CKMI_X4|OSCIn_IHRCO);
+#else
+	// SysClk=22.1184MHz CpuClk=11.0592MHz
+	CLK_SetCKCON0(IHRCO_110592MHz|CPUCLK_SYSCLK_DIV_2|SYSCLK_MCKDO_DIV_1|ENABLE_CKM|CKM_OSCIN_DIV_2);
+	DelayXus(100);
+	// IHRCO, MCK=CKMIx4, OSCin=IHRCO
+	CLK_SetCKCON2(ENABLE_IHRCO|MCK_CKMI_X4|OSCIn_IHRCO);
+#endif
+#endif
+
+#if (MCU_SYSCLK==24000000)
+#if (MCU_CPUCLK==MCU_SYSCLK)
+	// SysClk=24MHz CpuClk=24MHz
+	CLK_SetCKCON0(IHRCO_12MHz|CPUCLK_SYSCLK_DIV_1|SYSCLK_MCKDO_DIV_1|ENABLE_CKM|CKM_OSCIN_DIV_2);
+	DelayXus(100);
+	// IHRCO, MCK=CKMIx4, OSCin=IHRCO
+	CLK_SetCKCON2(ENABLE_IHRCO|MCK_CKMI_X4|OSCIn_IHRCO);
+#else
+	// SysClk=24MHz CpuClk=12MHz
+	CLK_SetCKCON0(IHRCO_12MHz|CPUCLK_SYSCLK_DIV_2|SYSCLK_MCKDO_DIV_1|ENABLE_CKM|CKM_OSCIN_DIV_2);
+	DelayXus(100);
+	// IHRCO, MCK=CKMIx4, OSCin=IHRCO
+	CLK_SetCKCON2(ENABLE_IHRCO|MCK_CKMI_X4|OSCIn_IHRCO);
+#endif
+#endif
+
+#if (MCU_SYSCLK==29491200)
+#if (MCU_CPUCLK==MCU_SYSCLK)
+	// SysClk=29.491200MHz CpuClk=29.491200MHz
+	CLK_SetCpuCLK_HighSpeed();
+	// SysClk=29.491200MHz CpuClk=29.491200MHz
+	CLK_SetCKCON0(IHRCO_110592MHz|CPUCLK_SYSCLK_DIV_1|SYSCLK_MCKDO_DIV_1|ENABLE_CKM|CKM_OSCIN_DIV_2);
+	DelayXus(100);
+	// IHRCO, MCK=CKMIx5.33, OSCin=IHRCO
+	CLK_SetCKCON2(ENABLE_IHRCO|MCK_CKMI_X533|OSCIn_IHRCO);
+#else
+	// SysClk=29.491200MHz CpuClk=14.7456MHz
+	CLK_SetCKCON0(IHRCO_110592MHz|CPUCLK_SYSCLK_DIV_2|SYSCLK_MCKDO_DIV_1|ENABLE_CKM|CKM_OSCIN_DIV_2);
+	DelayXus(100);
+	// IHRCO, MCK=CKMIx5.33, OSCin=IHRCO
+	CLK_SetCKCON2(ENABLE_IHRCO|MCK_CKMI_X533|OSCIn_IHRCO);
+#endif
+#endif
+
+#if (MCU_SYSCLK==32000000)
+#if (MCU_CPUCLK==MCU_SYSCLK)
+	// SysClk=32MHz CpuClk=32MHz
+	CLK_SetCpuCLK_HighSpeed();
+	// SysClk=32MHz CpuClk=32MHz
+	CLK_SetCKCON0(IHRCO_12MHz|CPUCLK_SYSCLK_DIV_1|SYSCLK_MCKDO_DIV_1|ENABLE_CKM|CKM_OSCIN_DIV_2);
+	DelayXus(100);
+	// IHRCO, MCK=CKMIx5.33, OSCin=IHRCO
+	CLK_SetCKCON2(ENABLE_IHRCO|MCK_CKMI_X533|OSCIn_IHRCO);
+#else
+	// SysClk=32MHz CpuClk=16MHz
+	CLK_SetCKCON0(IHRCO_12MHz|CPUCLK_SYSCLK_DIV_2|SYSCLK_MCKDO_DIV_1|ENABLE_CKM|CKM_OSCIN_DIV_2);
+	DelayXus(100);
+	// IHRCO, MCK=CKMIx5.33, OSCin=IHRCO
+	CLK_SetCKCON2(ENABLE_IHRCO|MCK_CKMI_X533|OSCIn_IHRCO);
+#endif
+#endif
+
+#if (MCU_SYSCLK==36000000)
+#if (MCU_CPUCLK==MCU_SYSCLK)
+	// Cpuclk high speed
+	CLK_SetCpuCLK_HighSpeed();
+	// CKMIx6,x8,x12
+	CLK_SetCKM_x6x8x12();	
+	// SysClk=36MHz CpuClk=18MHz
+	CLK_SetCKCON0(IHRCO_12MHz|CPUCLK_SYSCLK_DIV_1|SYSCLK_MCKDO_DIV_1|ENABLE_CKM|CKM_OSCIN_DIV_2);
+	DelayXus(100);
+	// IHRCO, MCK=CKMIx6, OSCin=IHRCO
+	CLK_SetCKCON2(ENABLE_IHRCO|MCK_CKMI_X4_X6|OSCIn_IHRCO);
+#else
+	// CKMIx6,x8,x12
+	CLK_SetCKM_x6x8x12();	
+	// SysClk=36MHz CpuClk=18MHz
+	CLK_SetCKCON0(IHRCO_12MHz|CPUCLK_SYSCLK_DIV_2|SYSCLK_MCKDO_DIV_1|ENABLE_CKM|CKM_OSCIN_DIV_2);
+	DelayXus(100);
+	// IHRCO, MCK=CKMIx6, OSCin=IHRCO
+	CLK_SetCKCON2(ENABLE_IHRCO|MCK_CKMI_X4_X6|OSCIn_IHRCO);
+#endif
+#endif
+
+#if (MCU_SYSCLK==44236800)
+	// SysClk=44.2368MHz CpuClk=22.1184MHz
+	CLK_SetCKCON0(IHRCO_110592MHz|CPUCLK_SYSCLK_DIV_1|SYSCLK_MCKDO_DIV_1|ENABLE_CKM|CKM_OSCIN_DIV_2);
+	DelayXus(100);
+	// IHRCO, MCK=CKMIx8, OSCin=IHRCO
+	CLK_SetCKCON2(ENABLE_IHRCO|MCK_CKMI_X8|OSCIn_IHRCO);
+#endif
+
+#if (MCU_SYSCLK==48000000)
+	// SysClk=48MHz CpuClk=24MHz
+	CLK_SetCKCON0(IHRCO_12MHz|CPUCLK_SYSCLK_DIV_2|SYSCLK_MCKDO_DIV_1|ENABLE_CKM|CKM_OSCIN_DIV_2);
+	DelayXus(100);
+	// IHRCO, MCK=CKMIx8, OSCin=IHRCO
+	CLK_SetCKCON2(ENABLE_IHRCO|MCK_CKMI_X8|OSCIn_IHRCO);
+#endif
+
+	// P60 Output MCK/4
+	//CLK_P60OC_MCKDiv4();
+}
+
+
+
+
+///***********************************************************************************
+//Function:       void InitSystem(void)
+//Description:    Initialize MCU
+//Input:   
+//Output:     
+//*************************************************************************************/
+void InitSystem(void)
+{
+	InitClock();
+	InitPort();
+	InitPCA_PWM();
+
+}
+
+
+void main()
+{
+	WordTypeDef duty;
+
+    InitSystem();
+	
+	LED_G_0=0;LED_R=0;LED_G_1=0;
+    DelayXms(1000);
+	LED_G_0=1;LED_R=1;LED_G_1=1;
+
+	wDuty.W=PWM_2_4;
+	DutyFlag=0x00;
+	while(1)
+    {
+    	DelayXms(200);
+		LED_G_0=!LED_G_0;
+		if(DutyFlag==0)
+		{
+			wDuty.W=wDuty.W+20;
+			if(wDuty.W >= PWM_MAX)
+			{
+				wDuty.W = PWM_MAX;
+				DutyFlag=1;
+			}
+		}
+		else
+		{
+			if(wDuty.W < 21)
+			{
+				wDuty.W = PWM_MIN;
+				DutyFlag=0;
+			}
+			else
+			{
+				wDuty.W=wDuty.W-20;
+			}
+		}
+		
+		duty.W=PCA_C-wDuty.W;
+		// Buffer mode,CH2/CH3 reload by CH3
+		PCA_CH3_SetValue(duty.B.BHigh,duty.B.BLow);
+    }
+}
+
