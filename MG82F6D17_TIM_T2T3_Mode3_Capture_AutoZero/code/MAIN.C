d2033f00d890116048e297f0b30920b6605a63f5
Dracoit <52192140+Dracoit@users.noreply.github.com>
2025-12-07 00:45:23 +0530

    Add files via upload

---
+++ MG82F6D17_TIM_T2T3_Mode3_Capture_AutoZero/code/MAIN.C
@@ -0,0 +1,575 @@
+///*********************************************************************
+//    Project:MG82F6D17-DEMO
+//    Author:LZD
+//			MG82F6D17 SSOP20_V10 EV Board (TH194A)
+//			CpuCLK=32MHz, SysCLK=32MHz
+//		Description:
+//			T2 mode: capture with auto-zero
+//			T3 mode: capture with auto-zero 
+//		Note:
+//
+//    Creat time::
+//    Modify::
+//
+//*********************************************************************/
+#define _MAIN_C
+
+#include <Intrins.h>
+#include <Absacc.h>
+
+#include <Stdio.h>  // for printf
+
+#include ".\include\REG_MG82F6D17.H"
+#include ".\include\Type.h"
+#include ".\include\API_Macro_MG82F6D17.H"
+#include ".\include\API_Uart_BRGRL_MG82F6D17.H"
+
+
+///*************************************************
+//Set SysClk (MAX.50MHz) (MAX.50MHz)
+//Selection: 
+//	11059200,12000000,
+//	22118400,24000000,
+//	29491200,32000000,
+//	44236800,48000000
+//*************************************************/
+#define MCU_SYSCLK		32000000
+/*************************************************/
+///*************************************************
+//Set  CpuClk (MAX.36MHz)
+//	1) CpuCLK=SysCLK
+//	2) CpuClk=SysClk/2
+//*************************************************/
+#define MCU_CPUCLK		(MCU_SYSCLK)
+//#define MCU_CPUCLK		(MCU_SYSCLK/2)
+
+#define TIMER_1T_1ms_TH	((65536-(u16)(float)(1000*((float)(MCU_SYSCLK)/(float)(1000000)))) /256) 			
+#define TIMER_1T_1ms_TL	((65536-(u16)(float)(1000*((float)(MCU_SYSCLK)/(float)(1000000)))) %256)
+
+#define TIMER_12T_1ms_TH	((65536-(u16)(float)(1000*((float)(MCU_SYSCLK)/(float)(12000000)))) /256) 			
+#define TIMER_12T_1ms_TL	((65536-(u16)(float)(1000*((float)(MCU_SYSCLK)/(float)(12000000)))) %256)
+
+#define LED_G_0		P33
+#define LED_R		P34
+#define LED_G_1		P35
+
+#define CAPTURE_BUF_SIZE	8
+bit bT2Capture;
+xdata WordTypeDef wT2CaptureData[CAPTURE_BUF_SIZE];
+u8 T2CaptureCnt;
+
+bit bT3Capture;
+xdata WordTypeDef wT3CaptureData[CAPTURE_BUF_SIZE];
+u8 T3CaptureCnt;
+
+///***********************************************************************************
+//Function:   	void INT_T2(void)
+//Description:	T2 Interrupt handler
+//Input:   
+//Output:     
+//*************************************************************************************/
+void INT_T2(void) interrupt INT_VECTOR_T2
+{
+	_push_(SFRPI);
+	SFRPI=0;			
+	if(TF2)
+	{
+		LED_G_0=!LED_G_0;
+		TF2=0;
+	}
+	else if(EXF2)
+	{
+		EXF2=0;
+		if(bT2Capture==0)
+		{
+			wT2CaptureData[T2CaptureCnt].B.BLow=RCAP2L;
+			wT2CaptureData[T2CaptureCnt].B.BHigh=RCAP2H;
+			T2CaptureCnt++;
+			if(T2CaptureCnt>=CAPTURE_BUF_SIZE)
+			{
+				bT2Capture=TRUE;
+			}
+		}
+	}
+	_pop_(SFRPI);
+}
+
+///***********************************************************************************
+//Function:   	void INT_T0(void)
+//Description:	T3 Interrupt handler
+//Input:   
+//Output:     
+//*************************************************************************************/
+void INT_T3(void) interrupt INT_VECTOR_T3
+{
+	_push_(SFRPI);
+	SFRPI=1;				
+	if(TF3)
+	{
+		LED_G_1=!LED_G_1;
+		TF3=0;
+	}
+	else if(EXF3)
+	{
+		EXF3=0;
+		if(bT3Capture==0)
+		{
+			wT3CaptureData[T3CaptureCnt].B.BLow=RCAP3L;
+			wT3CaptureData[T3CaptureCnt].B.BHigh=RCAP3H;
+			T3CaptureCnt++;
+			if(T3CaptureCnt>=CAPTURE_BUF_SIZE)
+			{
+				bT3Capture=TRUE;
+			}
+		}
+	}
+	_pop_(SFRPI);
+}
+
+/***********************************************************************************
+
+///*************************************************
+//Function:     	void DelayXus(u16 xUs)
+//Description:   	dealy, unit:us
+//Input:     		u8 Us -> *1us  (1~255)
+//Output:     
+//*************************************************/
+void DelayXus(u8 xUs)
+{
+	while(xUs!=0)
+	{
+#if (MCU_CPUCLK>=11059200)
+		_nop_();
+#endif
+#if (MCU_CPUCLK>=14745600)
+		_nop_();
+		_nop_();
+		_nop_();
+		_nop_();
+#endif
+#if (MCU_CPUCLK>=16000000)
+		_nop_();
+#endif
+
+#if (MCU_CPUCLK>=22118400)
+		_nop_();
+		_nop_();
+		_nop_();
+		_nop_();
+		_nop_();
+		_nop_();
+#endif
+#if (MCU_CPUCLK>=24000000)
+		_nop_();
+		_nop_();
+#endif		
+#if (MCU_CPUCLK>=29491200)
+		_nop_();
+		_nop_();
+		_nop_();
+		_nop_();
+		_nop_();
+		_nop_();
+#endif
+#if (MCU_CPUCLK>=32000000)
+		_nop_();
+		_nop_();
+#endif
+
+		xUs--;
+	}
+}
+
+///*************************************************
+//Function:     	void DelayXms(u16 xMs)
+//Description:    dealy, unit:ms
+//Input:     		u16 xMs -> *1ms  (1~65535)
+//Output:     
+//*************************************************/
+void DelayXms(u16 xMs)
+{
+	while(xMs!=0)
+	{
+		CLRWDT();
+		DelayXus(200);
+		DelayXus(200);
+		DelayXus(200);
+		DelayXus(200);
+		DelayXus(200);
+		xMs--;
+		
+	}
+}
+
+///***********************************************************************************
+//Function:		void SendByte(u8 tByte)
+//Description:	send byte
+//Input:			u8 tByte: the data to be send
+//Output:     
+//*************************************************************************************/
+void SendByte(u8 tByte)
+{
+	bit bES0;
+	bES0=ES0;
+	ES0=0;
+	S0BUF=tByte;
+	while(TI0==0);
+	TI0=0;
+	ES0=bES0;
+}
+
+
+
+///***********************************************************************************
+//Function:		void SendStr(BYTE* PStr)
+//Description:	send string
+//Input: 			u8* PStr:the string to be send
+//Output:     
+//*************************************************************************************/
+void SendStr(u8* PStr)
+{
+	while(*PStr != 0)
+	{
+		SendByte(*PStr);
+		PStr ++;
+	}
+}
+
+///***********************************************************************************
+//Function:   	void SendHex(u8 ToSend)
+//Description:	send data for HEX
+//Input: 			u8 ToSend: the data to be send
+//Output:     
+//*************************************************************************************/
+void SendHex(u8 ToSend)
+{
+	u8 x;
+//	SendByte(' ');
+//	SendByte('0');
+//	SendByte('x');
+	x = ToSend >> 4;
+	if(x < 10)
+	{
+		SendByte('0'+x);
+	}
+	else
+	{
+		SendByte('A'-10+x);
+	}
+	x= ToSend&0x0F;
+	if(x < 10)
+	{
+		SendByte('0'+x);
+	}
+	else
+	{
+		SendByte('A'-10+x);
+	}
+}
+
+///***********************************************************************************
+//Function:   	void InitTimer2(void)
+//Description:	Initialize Timer2
+//Input:   
+//Output:     
+//*************************************************************************************/
+void InitTimer2(void)
+{
+	TM_SetT2Mode_CapturewithAutoZero();		// T2 mode: capture with Auto-Zero
+	TM_SetT2Clock_SYSCLKDiv12();			// T2 clock source: SYSCLK/12
+
+	TM_SetT2LowByte(0); 					// T2 low byte
+	TM_SetT2HighByte(0);					// T2 high byte
+	TM_SetT2RLHighByte(0);					// T2 reload high byte
+	TM_SetT2RLLowByte(0);					// T2 reload low byte
+
+	TM_SetT2Capture_T2EXPin();				// T2 EXI: T2EX pin
+	TM_SetT2CKOT2EX_P10P11();				// T2EX pin: P11
+	TM_EnableT2EX_DetectFalling();			// T2EX detect: Falling
+
+	TM_EnableT2();							// Enable T2
+}
+
+///***********************************************************************************
+//Function:   	void InitTimer3(void)
+//Description:	Initialize Timer3
+//Input:   
+//Output:     
+//*************************************************************************************/
+void InitTimer3(void)
+{
+	TM_SetT3Mode_CapturewithAutoZero();		// T3 mode: capture with Auto-Zero
+	TM_SetT3Clock_SYSCLKDiv12();			// T3 clock source: SYSCLK/12
+
+	TM_SetT3LowByte(0);						// T3 low byte
+	TM_SetT3HighByte(0);					// T3 high byte
+	TM_SetT3RLHighByte(0);					// T3 reload high byte
+	TM_SetT3RLLowByte(0);					// T3 reload low byte
+	
+	TM_SetT3Capture_P60();					// T3 EXI: P60
+	TM_EnableT3EX_DetectFalling();			// T3 Detect: Falling
+	TM_EnableT3();							// Enable T3
+
+}
+
+///***********************************************************************************
+//Function:   	void InitInterrupt()
+//Description:	Initialize Interrupt
+//Input:   
+//Output:   		
+//*************************************************************************************/
+void InitInterrupt(void)
+{
+	INT_EnTIMER2();
+	INT_EnTIMER3();
+	
+}
+
+///***********************************************************************************
+//Function:   	void InitPort()
+//Description:	Initialize IO Port
+//Input:   
+//Output:   		
+//*************************************************************************************/
+void InitPort(void)
+{
+	PORT_SetP3QuasiBi(BIT0|BIT1|BIT3|BIT4|BIT5);		// set P30,P31,P33,P34,P35 as Quasi-Bidirectional
+	PORT_SetP1OpenDrainPu(BIT1);						// set P11 as open-drain with pull-high for T2EX
+	PORT_SetP6OpenDrainPu(BIT0);						// set P60 as open-drain with pull-high for T3 capture
+}
+
+///***********************************************************************************
+//Function:   	void InitClock()
+//Description:	Initialize clock
+//Input:   
+//Output:   		
+//*************************************************************************************/
+void InitClock(void)
+{
+#if (MCU_SYSCLK==11059200)
+#if (MCU_CPUCLK==MCU_SYSCLK)
+	// SysClk=11.0592MHz CpuClk=11.0592MHz
+	CLK_SetCKCON0(IHRCO_110592MHz|CPUCLK_SYSCLK_DIV_1|SYSCLK_MCKDO_DIV_1);
+	
+#else
+	// SysClk=11.0592MHz CpuClk=5.5296MHz
+	CLK_SetCKCON0(IHRCO_110592MHz|CPUCLK_SYSCLK_DIV_2|SYSCLK_MCKDO_DIV_1);
+#endif
+#endif
+
+#if (MCU_SYSCLK==12000000)
+#if (MCU_CPUCLK==MCU_SYSCLK)
+	// SysClk=12MHz CpuClk=12MHz
+	CLK_SetCKCON0(IHRCO_12MHz|CPUCLK_SYSCLK_DIV_1|SYSCLK_MCKDO_DIV_1);
+	
+#else
+	// SysClk=12MHz CpuClk=6MHz
+	CLK_SetCKCON0(IHRCO_12MHz|CPUCLK_SYSCLK_DIV_2|SYSCLK_MCKDO_DIV_1);
+#endif
+#endif
+
+#if (MCU_SYSCLK==22118400)
+#if (MCU_CPUCLK==MCU_SYSCLK)
+	// SysClk=22.1184MHz CpuClk=22.1184MHz
+	CLK_SetCKCON0(IHRCO_110592MHz|CPUCLK_SYSCLK_DIV_1|SYSCLK_MCKDO_DIV_1|ENABLE_CKM|CKM_OSCIN_DIV_2);
+	DelayXus(100);
+	// IHRCO, MCK=CKMIx4, OSCin=IHRCO
+	CLK_SetCKCON2(ENABLE_IHRCO|MCK_CKMI_X4|OSCIn_IHRCO);
+#else
+	// SysClk=22.1184MHz CpuClk=11.0592MHz
+	CLK_SetCKCON0(IHRCO_110592MHz|CPUCLK_SYSCLK_DIV_2|SYSCLK_MCKDO_DIV_1|ENABLE_CKM|CKM_OSCIN_DIV_2);
+	DelayXus(100);
+	// IHRCO, MCK=CKMIx4, OSCin=IHRCO
+	CLK_SetCKCON2(ENABLE_IHRCO|MCK_CKMI_X4|OSCIn_IHRCO);
+#endif
+#endif
+
+#if (MCU_SYSCLK==24000000)
+#if (MCU_CPUCLK==MCU_SYSCLK)
+	// SysClk=24MHz CpuClk=24MHz
+	CLK_SetCKCON0(IHRCO_12MHz|CPUCLK_SYSCLK_DIV_1|SYSCLK_MCKDO_DIV_1|ENABLE_CKM|CKM_OSCIN_DIV_2);
+	DelayXus(100);
+	// IHRCO, MCK=CKMIx4, OSCin=IHRCO
+	CLK_SetCKCON2(ENABLE_IHRCO|MCK_CKMI_X4|OSCIn_IHRCO);
+#else
+	// SysClk=24MHz CpuClk=12MHz
+	CLK_SetCKCON0(IHRCO_12MHz|CPUCLK_SYSCLK_DIV_2|SYSCLK_MCKDO_DIV_1|ENABLE_CKM|CKM_OSCIN_DIV_2);
+	DelayXus(100);
+	// IHRCO, MCK=CKMIx4, OSCin=IHRCO
+	CLK_SetCKCON2(ENABLE_IHRCO|MCK_CKMI_X4|OSCIn_IHRCO);
+#endif
+#endif
+
+#if (MCU_SYSCLK==29491200)
+#if (MCU_CPUCLK==MCU_SYSCLK)
+	// Cpuclk high speed
+	CLK_SetCpuCLK_HighSpeed();
+	// SysClk=29.491200MHz CpuClk=29.491200MHz
+	CLK_SetCKCON0(IHRCO_110592MHz|CPUCLK_SYSCLK_DIV_1|SYSCLK_MCKDO_DIV_1|ENABLE_CKM|CKM_OSCIN_DIV_2);
+	DelayXus(100);
+	// IHRCO, MCK=CKMIx5.33, OSCin=IHRCO
+	CLK_SetCKCON2(ENABLE_IHRCO|MCK_CKMI_X533|OSCIn_IHRCO);
+#else
+	// SysClk=29.491200MHz CpuClk=14.7456MHz
+	CLK_SetCKCON0(IHRCO_110592MHz|CPUCLK_SYSCLK_DIV_2|SYSCLK_MCKDO_DIV_1|ENABLE_CKM|CKM_OSCIN_DIV_2);
+	DelayXus(100);
+	// IHRCO, MCK=CKMIx5.33, OSCin=IHRCO
+	CLK_SetCKCON2(ENABLE_IHRCO|MCK_CKMI_X533|OSCIn_IHRCO);
+#endif
+#endif
+
+#if (MCU_SYSCLK==32000000)
+#if (MCU_CPUCLK==MCU_SYSCLK)
+	// Cpuclk high speed
+	CLK_SetCpuCLK_HighSpeed();
+	// SysClk=32MHz CpuClk=32MHz
+	CLK_SetCKCON0(IHRCO_12MHz|CPUCLK_SYSCLK_DIV_1|SYSCLK_MCKDO_DIV_1|ENABLE_CKM|CKM_OSCIN_DIV_2);
+	DelayXus(100);
+	// IHRCO, MCK=CKMIx5.33, OSCin=IHRCO
+	CLK_SetCKCON2(ENABLE_IHRCO|MCK_CKMI_X533|OSCIn_IHRCO);
+#else
+	// SysClk=32MHz CpuClk=16MHz
+	CLK_SetCKCON0(IHRCO_12MHz|CPUCLK_SYSCLK_DIV_2|SYSCLK_MCKDO_DIV_1|ENABLE_CKM|CKM_OSCIN_DIV_2);
+	DelayXus(100);
+	// IHRCO, MCK=CKMIx5.33, OSCin=IHRCO
+	CLK_SetCKCON2(ENABLE_IHRCO|MCK_CKMI_X533|OSCIn_IHRCO);
+#endif
+#endif
+
+#if (MCU_SYSCLK==36000000)
+#if (MCU_CPUCLK==MCU_SYSCLK)
+	// Cpuclk high speed
+	CLK_SetCpuCLK_HighSpeed();
+	// CKMIx6,x8,x12
+	CLK_SetCKM_x6x8x12();	
+	// SysClk=36MHz CpuClk=18MHz
+	CLK_SetCKCON0(IHRCO_12MHz|CPUCLK_SYSCLK_DIV_1|SYSCLK_MCKDO_DIV_1|ENABLE_CKM|CKM_OSCIN_DIV_2);
+	DelayXus(100);
+	// IHRCO, MCK=CKMIx6, OSCin=IHRCO
+	CLK_SetCKCON2(ENABLE_IHRCO|MCK_CKMI_X4_X6|OSCIn_IHRCO);
+#else
+	// CKMIx6,x8,x12
+	CLK_SetCKM_x6x8x12();	
+	// SysClk=36MHz CpuClk=18MHz
+	CLK_SetCKCON0(IHRCO_12MHz|CPUCLK_SYSCLK_DIV_2|SYSCLK_MCKDO_DIV_1|ENABLE_CKM|CKM_OSCIN_DIV_2);
+	DelayXus(100);
+	// IHRCO, MCK=CKMIx6, OSCin=IHRCO
+	CLK_SetCKCON2(ENABLE_IHRCO|MCK_CKMI_X4_X6|OSCIn_IHRCO);
+#endif
+#endif
+
+
+#if (MCU_SYSCLK==44236800)
+	// SysClk=44.2368MHz CpuClk=22.1184MHz
+	CLK_SetCKCON0(IHRCO_110592MHz|CPUCLK_SYSCLK_DIV_1|SYSCLK_MCKDO_DIV_1|ENABLE_CKM|CKM_OSCIN_DIV_2);
+	DelayXus(100);
+	// IHRCO, MCK=CKMIx8, OSCin=IHRCO
+	CLK_SetCKCON2(ENABLE_IHRCO|MCK_CKMI_X8|OSCIn_IHRCO);
+#endif
+
+#if (MCU_SYSCLK==48000000)
+	// SysClk=48MHz CpuClk=24MHz
+	CLK_SetCKCON0(IHRCO_12MHz|CPUCLK_SYSCLK_DIV_2|SYSCLK_MCKDO_DIV_1|ENABLE_CKM|CKM_OSCIN_DIV_2);
+	DelayXus(100);
+	// IHRCO, MCK=CKMIx8, OSCin=IHRCO
+	CLK_SetCKCON2(ENABLE_IHRCO|MCK_CKMI_X8|OSCIn_IHRCO);
+#endif
+
+	// P60 Output MCK/4
+	//CLK_P60OC_MCKDiv4();
+}
+
+
+
+///***********************************************************************************
+//Function:   	void InitUart0_S0BRG()
+//Description:	Initialize Uart0, The source of baud rate was S0BRG
+//Input:   
+//Output:   		
+//*************************************************************************************/
+void InitUart0_S0BRG(void)
+{
+	UART0_SetAccess_S0CR1();			// Enable access S0CR1 
+	UART0_SetMode8bitUARTVar();			// UART0 Mode: 8-bit, Variable B.R.
+	UART0_EnReception();				// Enable reception
+	UART0_SetBRGFromS0BRG();			// B.R. source: S0BRG
+	UART0_SetS0BRGBaudRateX2();			// S0BRG x2
+	UART0_SetS0BRGSelSYSCLK();			// S0BRG clock source: SYSCLK
+
+	// Sets B.R. value
+	UART0_SetS0BRGValue(S0BRG_BRGRL_57600_2X_32000000_1T);
+
+	UART0_EnS0BRG();					// Enable S0BRG
+}
+
+
+
+///***********************************************************************************
+//Function:       void InitSystem(void)
+//Description:    Initialize MCU
+//Input:   
+//Output:     
+//*************************************************************************************/
+void InitSystem(void)
+{
+	
+	InitPort();
+	InitClock();
+	InitUart0_S0BRG();
+	InitTimer2();			
+	InitTimer3();			
+
+	InitInterrupt();		
+	
+	INT_EnAll();						// Enable global interrupt
+
+}
+
+
+
+void main()
+{
+	u8 i;
+	
+    InitSystem();
+	
+ 	LED_G_0=0;LED_G_1=0;LED_R=0;
+	DelayXms(1000);
+ 	LED_G_0=1;LED_G_1=1;LED_R=1;
+
+					
+    while(1)
+    {
+    	DelayXms(100);
+    	LED_R=!LED_R;
+    	if(bT2Capture)
+    	{
+			SendStr("\nT2 Caputre:");
+			for(i=0;i<CAPTURE_BUF_SIZE;i++)
+			{
+				SendByte('0');
+				SendByte('x');
+				SendHex(wT2CaptureData[i].B.BHigh);
+				SendHex(wT2CaptureData[i].B.BLow);
+				SendByte(' ');
+			}
+			T2CaptureCnt=0;
+			bT2Capture=0;
+    	}
+		if(bT3Capture)
+		{
+			SendStr("\nT3 Caputre:");
+			for(i=0;i<CAPTURE_BUF_SIZE;i++)
+			{
+				SendByte('0');
+				SendByte('x');
+				SendHex(wT3CaptureData[i].B.BHigh);
+				SendHex(wT3CaptureData[i].B.BLow);
+				SendByte(' ');
+			}
+			T3CaptureCnt=0;
+			bT3Capture=0;
+    	}
+    }
+}
+
