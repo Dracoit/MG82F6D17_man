81203c8b429aa17400875f96b7ac1d2609330761
Dracoit <52192140+Dracoit@users.noreply.github.com>
2025-12-07 00:49:57 +0530

    Add files via upload

---
+++ MG82F6D17_SPI_Slave_Int/code/MAIN.C
@@ -0,0 +1,361 @@
+///*********************************************************************
+//    Project:MG82F6D17-DEMO
+//    Author:LZD
+//			MG82F6D17 SSOP20_V10 EV Board (TH194A)
+//			CpuCLK=12MHz, SysCLK=12MHz
+//	Description:
+//			SPI(nSS/P33,MOSI/P15,MISO/P16,SPICLK/P17) 
+//			Slave 
+//	Note:
+//
+//    Creat time::
+//    Modify::
+//    
+//*********************************************************************/
+#define _MAIN_C
+
+#include <Intrins.h>
+#include <Absacc.h>
+
+#include <Stdio.h>  // for printf
+
+#include ".\include\REG_MG82F6D17.H"
+#include ".\include\Type.h"
+#include ".\include\API_Macro_MG82F6D17.H"
+#include ".\include\API_Uart_BRGRL_MG82F6D17.H"
+
+
+///*************************************************
+//Set SysClk (MAX.50MHz) (MAX.50MHz)
+//Selection: 
+//	11059200,12000000,
+//	22118400,24000000,
+//	29491200,32000000,
+//	44236800,48000000
+//*************************************************/
+#define MCU_SYSCLK		48000000
+/*************************************************/
+///*************************************************
+//set CpuClk (MAX.36MHz)
+//	1) CpuCLK=SysCLK
+//	2) CpuClk=SysClk/2
+//*************************************************/
+//#define MCU_CPUCLK		(MCU_SYSCLK)
+#define MCU_CPUCLK		(MCU_SYSCLK/2)
+
+#define TIMER_1T_1ms_TH	((65536-(u16)(float)(1000*((float)(MCU_SYSCLK)/(float)(1000000)))) /256) 			
+#define TIMER_1T_1ms_TL	((65536-(u16)(float)(1000*((float)(MCU_SYSCLK)/(float)(1000000)))) %256)
+
+#define TIMER_12T_1ms_TH	((65536-(u16)(float)(1000*((float)(MCU_SYSCLK)/(float)(12000000)))) /256) 			
+#define TIMER_12T_1ms_TL	((65536-(u16)(float)(1000*((float)(MCU_SYSCLK)/(float)(12000000)))) %256)
+
+#define LED_G_0		P33
+#define LED_R		P34
+#define LED_G_1		P35
+
+
+#define SPI_nSS		P33
+
+
+///***********************************************************************************
+//Function:   void INT_SPI(void)
+//Description:SPI Interrupt handler
+//		 
+//Input:   
+//Output:     
+//*************************************************************************************/
+void INT_SPI() interrupt INT_VECTOR_SPI
+{
+	BYTE i;
+	i=SPDAT;				// read current SPI data
+	SPSTAT = SPSTAT |SPIF;	// clear flag
+	SPDAT=i+1; 				// SPI data +1£¨for next trans
+}
+
+
+///*************************************************
+//Function:     	void DelayXus(u16 xUs)
+//Description:   	dealy, unit:us
+//Input:     		u8 Us -> *1us  (1~255)
+//Output:     
+//*************************************************/
+void DelayXus(u8 xUs)
+{
+	while(xUs!=0)
+	{
+#if (MCU_CPUCLK>=11059200)
+		_nop_();
+#endif
+#if (MCU_CPUCLK>=14745600)
+		_nop_();
+		_nop_();
+		_nop_();
+		_nop_();
+#endif
+#if (MCU_CPUCLK>=16000000)
+		_nop_();
+#endif
+
+#if (MCU_CPUCLK>=22118400)
+		_nop_();
+		_nop_();
+		_nop_();
+		_nop_();
+		_nop_();
+		_nop_();
+#endif
+#if (MCU_CPUCLK>=24000000)
+		_nop_();
+		_nop_();
+#endif		
+#if (MCU_CPUCLK>=29491200)
+		_nop_();
+		_nop_();
+		_nop_();
+		_nop_();
+		_nop_();
+		_nop_();
+#endif
+#if (MCU_CPUCLK>=32000000)
+		_nop_();
+		_nop_();
+#endif
+
+		xUs--;
+	}
+}
+
+///*************************************************
+//Function:     	void DelayXms(u16 xMs)
+//Description:    dealy, unit:ms
+//Input:     		u16 xMs -> *1ms  (1~65535)
+//Output:     
+//*************************************************/
+void DelayXms(u16 xMs)
+{
+	while(xMs!=0)
+	{
+		CLRWDT();
+		DelayXus(200);
+		DelayXus(200);
+		DelayXus(200);
+		DelayXus(200);
+		DelayXus(200);
+		xMs--;
+		
+	}
+}
+
+///***********************************************************************************
+//Function:   	void InitPort()
+//Description:	Initialize IO Port
+//Input:   
+//Output:   		
+//*************************************************************************************/
+void InitPort(void)
+{
+	PORT_SetP1OpenDrain(BIT5|BIT7);					// Set P15(MOSI),P17(SPICLK) as open-drain for SPICLK/MOSI
+	PORT_SetP1PushPull(BIT6);						// Set P16(MISO) as push-pull for MISO
+	PORT_SetP3QuasiBi(BIT3|BIT4|BIT5);				// Set P33,P34,P35 as Quasi-Bidirectional
+//	PORT_P14P15P16P17_HighDrive();  
+//	PORT_P1_EnFastDrive(BIT4|BIT5|BIT6|BIT7);
+//	PORT_P3_EnFastDrive(BIT3);
+}
+
+
+///***********************************************************************************
+//Function:   void InitSPI_Slave(void)
+//Description:   SPI≥ı ºªØ slave
+//Input:   
+//Output:     
+//*************************************************************************************/
+void InitSPI_Slave(void)
+{
+	
+	SPI_Enable();									// Enable SPI
+	SPI_SelectMASTERSLAVEBynSSpin();				// Set MASTER/SLAVE by nSS pin
+	SPI_SetClock(SPI_CLK_SYSCLK_4);					// Set Clock SYSCLK/4 48M/4=12M
+	SPI_SetCPOL_0();								// CPOL=0 
+	SPI_SetDataMSB();								// Data MSB
+	SPI_SetCPHA_0();								// CPHA=0
+	//SPI_SetCPHA_1();								// CPHA=1
+	SPI_SetUseP33P15P16P17();						// IO Port: nSS/P33,MOSI/P15,MISO/P16,SPICLK/P17
+
+}
+
+///***********************************************************************************
+//Function:   	u8 SPITransceiver(u8 SPI_DATA)
+//Description:	SPI Master transmit 
+//Input:   		u8 SPI_DATA: Data to be send
+//Output:   		u8:  Received data
+//*************************************************************************************/
+u8 SPITransceiver(u8 SPI_DATA)
+{
+	SPI_SendData(SPI_DATA);							// Send data
+	while(SPI_ChkCompleteFlag()==0);				// Wait complete
+	SPI_ClearCompleteFlag();						// Clear flag
+	return SPI_GetData();							// Return data
+}
+
+///***********************************************************************************
+//Function:   	void InitInterrupt()
+//Description:	Initialize Interrupt
+//Input:   
+//Output:   		
+//*************************************************************************************/
+void InitInterrupt(void)
+{
+	INT_EnSPI();									// Enable SPI interrupt
+}	
+
+///***********************************************************************************
+//Function:   	void InitClock()
+//Description:	Initialize clock
+//Input:   
+//Output:   		
+//*************************************************************************************/
+void InitClock(void)
+{
+#if (MCU_SYSCLK==11059200)
+#if (MCU_CPUCLK==MCU_SYSCLK)
+	// SysClk=11.0592MHz CpuClk=11.0592MHz
+	CLK_SetCKCON0(IHRCO_110592MHz|CPUCLK_SYSCLK_DIV_1|SYSCLK_MCKDO_DIV_1);
+	
+#else
+	// SysClk=11.0592MHz CpuClk=5.5296MHz
+	CLK_SetCKCON0(IHRCO_110592MHz|CPUCLK_SYSCLK_DIV_2|SYSCLK_MCKDO_DIV_1);
+#endif
+#endif
+
+#if (MCU_SYSCLK==12000000)
+#if (MCU_CPUCLK==MCU_SYSCLK)
+	// SysClk=12MHz CpuClk=12MHz
+	CLK_SetCKCON0(IHRCO_12MHz|CPUCLK_SYSCLK_DIV_1|SYSCLK_MCKDO_DIV_1);
+	
+#else
+	// SysClk=12MHz CpuClk=6MHz
+	CLK_SetCKCON0(IHRCO_12MHz|CPUCLK_SYSCLK_DIV_2|SYSCLK_MCKDO_DIV_1);
+#endif
+#endif
+
+#if (MCU_SYSCLK==22118400)
+#if (MCU_CPUCLK==MCU_SYSCLK)
+	// SysClk=22.1184MHz CpuClk=22.1184MHz
+	CLK_SetCKCON0(IHRCO_110592MHz|CPUCLK_SYSCLK_DIV_1|SYSCLK_MCKDO_DIV_1|ENABLE_CKM|CKM_OSCIN_DIV_2);
+	DelayXus(100);
+	// IHRCO, MCK=CKMIx4, OSCin=IHRCO
+	CLK_SetCKCON2(ENABLE_IHRCO|MCK_CKMI_X4|OSCIn_IHRCO);
+#else
+	// SysClk=22.1184MHz CpuClk=11.0592MHz
+	CLK_SetCKCON0(IHRCO_110592MHz|CPUCLK_SYSCLK_DIV_2|SYSCLK_MCKDO_DIV_1|ENABLE_CKM|CKM_OSCIN_DIV_2);
+	DelayXus(100);
+	// IHRCO, MCK=CKMIx4, OSCin=IHRCO
+	CLK_SetCKCON2(ENABLE_IHRCO|MCK_CKMI_X4|OSCIn_IHRCO);
+#endif
+#endif
+
+#if (MCU_SYSCLK==24000000)
+#if (MCU_CPUCLK==MCU_SYSCLK)
+	// SysClk=24MHz CpuClk=24MHz
+	CLK_SetCKCON0(IHRCO_12MHz|CPUCLK_SYSCLK_DIV_1|SYSCLK_MCKDO_DIV_1|ENABLE_CKM|CKM_OSCIN_DIV_2);
+	DelayXus(100);
+	// IHRCO, MCK=CKMIx4, OSCin=IHRCO
+	CLK_SetCKCON2(ENABLE_IHRCO|MCK_CKMI_X4|OSCIn_IHRCO);
+#else
+	// SysClk=24MHz CpuClk=12MHz
+	CLK_SetCKCON0(IHRCO_12MHz|CPUCLK_SYSCLK_DIV_2|SYSCLK_MCKDO_DIV_1|ENABLE_CKM|CKM_OSCIN_DIV_2);
+	DelayXus(100);
+	// IHRCO, MCK=CKMIx4, OSCin=IHRCO
+	CLK_SetCKCON2(ENABLE_IHRCO|MCK_CKMI_X4|OSCIn_IHRCO);
+#endif
+#endif
+
+#if (MCU_SYSCLK==29491200)
+#if (MCU_CPUCLK==MCU_SYSCLK)
+	// Cpuclk high speed
+	CLK_SetCpuCLK_HighSpeed();
+	// SysClk=29.491200MHz CpuClk=29.491200MHz
+	CLK_SetCKCON0(IHRCO_110592MHz|CPUCLK_SYSCLK_DIV_1|SYSCLK_MCKDO_DIV_1|ENABLE_CKM|CKM_OSCIN_DIV_2);
+	DelayXus(100);
+	// IHRCO, MCK=CKMIx5.33, OSCin=IHRCO
+	CLK_SetCKCON2(ENABLE_IHRCO|MCK_CKMI_X533|OSCIn_IHRCO);
+#else
+	// SysClk=29.491200MHz CpuClk=14.7456MHz
+	CLK_SetCKCON0(IHRCO_110592MHz|CPUCLK_SYSCLK_DIV_2|SYSCLK_MCKDO_DIV_1|ENABLE_CKM|CKM_OSCIN_DIV_2);
+	DelayXus(100);
+	// IHRCO, MCK=CKMIx5.33, OSCin=IHRCO
+	CLK_SetCKCON2(ENABLE_IHRCO|MCK_CKMI_X533|OSCIn_IHRCO);
+#endif
+#endif
+
+#if (MCU_SYSCLK==32000000)
+#if (MCU_CPUCLK==MCU_SYSCLK)
+	// Cpuclk high speed
+	CLK_SetCpuCLK_HighSpeed();
+	// SysClk=32MHz CpuClk=32MHz
+	CLK_SetCKCON0(IHRCO_12MHz|CPUCLK_SYSCLK_DIV_1|SYSCLK_MCKDO_DIV_1|ENABLE_CKM|CKM_OSCIN_DIV_2);
+	DelayXus(100);
+	// IHRCO, MCK=CKMIx5.33, OSCin=IHRCO
+	CLK_SetCKCON2(ENABLE_IHRCO|MCK_CKMI_X533|OSCIn_IHRCO);
+#else
+	// SysClk=32MHz CpuClk=16MHz
+	CLK_SetCKCON0(IHRCO_12MHz|CPUCLK_SYSCLK_DIV_2|SYSCLK_MCKDO_DIV_1|ENABLE_CKM|CKM_OSCIN_DIV_2);
+	DelayXus(100);
+	// IHRCO, MCK=CKMIx5.33, OSCin=IHRCO
+	CLK_SetCKCON2(ENABLE_IHRCO|MCK_CKMI_X533|OSCIn_IHRCO);
+#endif
+#endif
+#if (MCU_SYSCLK==44236800)
+	// SysClk=44.2368MHz CpuClk=22.1184MHz
+	CLK_SetCKCON0(IHRCO_110592MHz|CPUCLK_SYSCLK_DIV_1|SYSCLK_MCKDO_DIV_1|ENABLE_CKM|CKM_OSCIN_DIV_2);
+	DelayXus(100);
+	// IHRCO, MCK=CKMIx8, OSCin=IHRCO
+	CLK_SetCKCON2(ENABLE_IHRCO|MCK_CKMI_X8|OSCIn_IHRCO);
+#endif
+
+#if (MCU_SYSCLK==48000000)
+	// SysClk=48MHz CpuClk=24MHz
+	CLK_SetCKCON0(IHRCO_12MHz|CPUCLK_SYSCLK_DIV_2|SYSCLK_MCKDO_DIV_1|ENABLE_CKM|CKM_OSCIN_DIV_2);
+	DelayXus(100);
+	// IHRCO, MCK=CKMIx8, OSCin=IHRCO
+	CLK_SetCKCON2(ENABLE_IHRCO|MCK_CKMI_X8|OSCIn_IHRCO);
+#endif
+
+	// P60 Output MCK/4
+	//CLK_P60OC_MCKDiv4();
+}
+
+///***********************************************************************************
+//Function:       void InitSystem(void)
+//Description:    Initialize MCU
+//Input:   
+//Output:     
+//*************************************************************************************/
+void InitSystem(void)
+{
+	InitPort();
+	InitClock();
+	InitSPI_Slave();
+	InitInterrupt();
+
+	INT_EnAll();			//  Enable global interrupt
+}
+
+
+void main()
+{
+    InitSystem();
+	
+	LED_G_1=0;LED_R=0;
+	DelayXms(1000);
+	LED_G_1=1;LED_R=1;
+
+    while(1)
+    {
+		DelayXms(100);
+		LED_R = !LED_R;
+
+    }
+}
+
+
